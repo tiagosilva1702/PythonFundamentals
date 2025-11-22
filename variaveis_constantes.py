29a3fa84b5b193e278a18cda970637178d2cea57
Tiago <tiago10.ba@hotmail.com>
2025-11-22 16:26:18 -0300

    Variáveis e Constantes

---
+++ variaveis_constantes.py
@@ -0,0 +1,17 @@
+# Variaveis: sugestão usar nomes separados por underline
+nome = "João"
+idade = 25  
+altura = 1.75
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+
+# Constantes: por convenção, usamos letras maiúsculas
+PI = 3.14159
+GRAVIDADE = 9.81
+print("Valor de PI:", PI)
+print("Valor da Gravidade:", GRAVIDADE)
+
+BRAZILIAN_STATES = ["São Paulo", "Rio de Janeiro", "Minas Gerais"]
+
+print("Estados do Brasil:", BRAZILIAN_STATES)
