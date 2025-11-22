d1622ab8cbd746783863ee4f8cbe404c76763377
Tiago <tiago10.ba@hotmail.com>
2025-11-22 16:42:36 -0300

    Conversão de Tipos

---
+++ conversao_tipos.py
@@ -0,0 +1,45 @@
+# inteiro para float
+preco = 10
+print(preco)
+
+preco = float(preco)
+print(preco)    
+
+preco = 10/2
+print(preco)
+
+#float para inteiro
+quantidade = 3.5
+print(quantidade)   
+
+quantidade = int(quantidade)
+print(quantidade)
+
+#conversao por divisão
+total = 10
+por_pessoa = total / 3
+print(por_pessoa)
+
+por_pessoa = int(total / 3)
+print(por_pessoa)  
+
+#conversao por parte inteira
+print(por_pessoa//3)
+
+#conversao por arredondamento
+por_pessoa = round(total / 3)
+print(por_pessoa)
+
+#conversao para string
+idade = 25
+print(str(idade))
+
+texto = f"A idade é {idade} anos."
+print(texto)
+
+#converter string para inteiro
+numero = "10"
+print(int(numero))
+
+#converter string para float
+numero = "10.5"
